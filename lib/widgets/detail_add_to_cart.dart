d6d05924ceacb25c934f162e614a764a15afef43
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-08-28 13:07:49 +0300

    flutter plant app ui

--- lib/widgets/detail_add_to_cart.dart
+++ lib/widgets/detail_add_to_cart.dart
@@ -29,7 +29,7 @@ class DetailAddToCart extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            color: Color.fromARGB(255, 77, 191, 149),
+            color: const Color.fromARGB(255, 77, 191, 149),
           ),
           child: const Text(
             "Add to Cart",
